805ce0db86792db96bc7d4682c09e5499a71d658
hamzahasa067 <hamzahasa067@gmail.com>
2023-01-19 14:07:56 +0200

    intial commit

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,36 @@
+import { Document, Model, Types } from "mongoose";
+
+// export interface ITodos {
+//   _id: Types.ObjectId;
+//   text: string;
+//   done: boolean;
+// }
+
+export interface ITokenData {
+  email: string;
+  name: string;
+}
+export interface IUserLoginData {
+  email: string;
+  password: string;
+}
+export interface ISavedCreatedUser {
+  email: string;
+  name: string;
+  encrypted_password: string;
+}
+export interface ICreateUserReqeustBody {
+  email?: string;
+  name?: string;
+  password?: string;
+}
+export interface IUser {
+  email: string;
+  name: string;
+  encrypted_password: string;
+}
+export interface IUserMethods {
+  compare(password: string): boolean;
+}
+
+export type UserModel = Model<IUser, {}, IUserMethods>;
---
+++ src/models/user.model.ts
@@ -0,0 +1,49 @@
+import { Schema, model } from "mongoose";
+import {
+  IUser,
+  UserModel,
+  IUserMethods,
+  // ITodos,
+} from "../interfaces/user.interface";
+import bcrypt from "bcrypt";
+import Debug from "../utils/Debug";
+
+// const TodosSchema = new Schema<ITodos>({
+//   text: {
+//     type: String,
+//     required: true,
+//   },
+//   done: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
+
+const UserSchema = new Schema<IUser, UserModel, IUserMethods>({
+  name: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+    unique: true,
+    lowercase: true,
+  },
+  encrypted_password: {
+    required: true,
+    type: String,
+  },
+  // todos: [TodosSchema],
+});
+UserSchema.method("compare", async function (password: string) {
+  try {
+    const valid: any = await bcrypt.compare(password, this.encrypted_password);
+    Debug.log(valid, this.encrypted_password, password);
+    return valid;
+  } catch (error: unknown) {
+    Debug.error("Error at compare function", error);
+    return false;
+  }
+});
+export default model<IUser, UserModel>("User", UserSchema);
